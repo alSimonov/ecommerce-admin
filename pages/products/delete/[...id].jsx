b2a207e79e091ae0514281f1661dd4d7bc77086b
alSimonov <al.simonov20168090@gmail.com>
2024-05-12 14:51:41 +0300

    aa

--- pages/products/delete/[...id].jsx
+++ pages/products/delete/[...id].jsx
@@ -26,7 +26,7 @@ export default function DeleteProductPage() {
 	}
 	return (
 		<Layout>
-			<h1 className="text-center">Вы уверены, что хотите удалить "{productInfo?.title}"?</h1>
+			<h1 className="text-center">Вы уверены, что хотите удалить |{productInfo?.title}|?</h1>
 			<div className="flex gap-2 justify-center">
 				<button 
 					onClick={deleteProduct}
