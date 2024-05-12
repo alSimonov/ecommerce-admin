0ac40770671dc63f5fd4c1d1d8ebb453796aad1d
alSimonov <al.simonov20168090@gmail.com>
2024-05-12 19:02:53 +0300

    add secret for nextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,6 +13,7 @@ export const authOptions = {
         clientSecret: process.env.YANDEX_CLIENT_SECRET
     }),
     ],
+    secret : process.env.NEXT_PUBLIC_SECRET,
     adapter: MongoDBAdapter(clientPromise, {
         Users: "ClientUsers",
         Accounts: "ClientAccounts",
